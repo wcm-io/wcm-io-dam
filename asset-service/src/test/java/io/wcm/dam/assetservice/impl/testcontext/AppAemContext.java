980e9cbdaed1a984659bfab1be25e80524a5eddb
sseifert <sseifert@pro-vision.de>
2016-12-05 18:15:24 +0100

    update mocks

--- asset-service/src/test/java/io/wcm/dam/assetservice/impl/testcontext/AppAemContext.java
+++ asset-service/src/test/java/io/wcm/dam/assetservice/impl/testcontext/AppAemContext.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,15 +19,19 @@
  */
 package io.wcm.dam.assetservice.impl.testcontext;
 
-import io.wcm.config.spi.ApplicationProvider;
-import io.wcm.handler.media.spi.MediaFormatProvider;
-import io.wcm.testing.mock.aem.junit.AemContext;
-import io.wcm.testing.mock.aem.junit.AemContextCallback;
-import io.wcm.testing.mock.wcmio.handler.MockHandler;
+import static io.wcm.testing.mock.wcmio.config.ContextPlugins.WCMIO_CONFIG;
+import static io.wcm.testing.mock.wcmio.handler.ContextPlugins.WCMIO_HANDLER;
+import static io.wcm.testing.mock.wcmio.sling.ContextPlugins.WCMIO_SLING;
 
 import java.io.IOException;
 
 import org.apache.sling.api.resource.PersistenceException;
+
+import io.wcm.config.spi.ApplicationProvider;
+import io.wcm.handler.media.spi.MediaFormatProvider;
+import io.wcm.testing.mock.aem.junit.AemContext;
+import io.wcm.testing.mock.aem.junit.AemContextBuilder;
+import io.wcm.testing.mock.aem.junit.AemContextCallback;
 
 /**
  * Sets up {@link AemContext} for unit tests in this application.
@@ -41,21 +45,21 @@ public final class AppAemContext {
   }
 
   public static AemContext newAemContext() {
-    return new AemContext(new SetUpCallback());
+    return new AemContextBuilder()
+        .plugin(WCMIO_SLING, WCMIO_CONFIG, WCMIO_HANDLER)
+        .afterSetUp(SETUP_CALLBACK)
+        .build();
   }
 
   /**
    * Custom set up rules required in all unit tests.
    */
-  private static final class SetUpCallback implements AemContextCallback {
-
+  private static final AemContextCallback SETUP_CALLBACK = new AemContextCallback() {
     @Override
     public void execute(AemContext context) throws PersistenceException, IOException {
       context.registerService(ApplicationProvider.class, new ApplicationProviderImpl());
       context.registerService(MediaFormatProvider.class, new MediaFormatProviderImpl());
-      MockHandler.setUp(context);
     }
-
-  }
+  };
 
 }
